56736f2a8b881542b361f74d49c348927e004c64
Nakavasi <119505794+Nakavasi@users.noreply.github.com>
2023-11-30 16:07:57 +0100

    feat
    Page qui affiche la recette demandé.

---
+++ AfficherRecette.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recette</title>
+</head>
+<body>
+    <h1><a href="Recette.php">🔙</a></h1>
+    <h1>Voici la recette de
+<?php
+include("connexion.php");
+$id= $_GET["ID"];
+$requete= $connexion->prepare("SELECT * FROM recettes WHERE id=?");
+$requete->execute([$id]);
+$recetes= $requete->fetch(PDO::FETCH_ASSOC);
+echo($recetes["nom"]." </h1>");
+echo("<h3>".$recetes["contenue"]."</h3>")
+?>  
+</body>
+</html>
