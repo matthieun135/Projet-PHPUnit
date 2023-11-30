f9ee45198fa29e4f93808f9ac30818e08cbf0378
Nakavasi <119505794+Nakavasi@users.noreply.github.com>
2023-11-30 16:09:35 +0100

    Feat
    Ajout d'un lien pour retourner a l'accueil

--- Recette.php
+++ Recette.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><a href="Accueil.php">Accueil</a></h2>
+</body>
+</html>
 <?php
 include("connexion.php");
 $requete= $connexion->query("SELECT * FROM recettes");
