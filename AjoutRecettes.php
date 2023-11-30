f3105fb4b5cf3ec930fe519caefee6a1dc0f7a2e
matthieun135 <110544980+matthieun135@users.noreply.github.com>
2023-11-30 15:12:05 +0100

    Add files via upload

---
+++ AjoutRecettes.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'config.php';
+
+// Récupérer les données du formulaire
+$id = $_POST['id'];
+$nom = $_POST['nom'];
+$difficulte = $_POST['difficulte'];
+$idcategorie = $_POST['idcategorie'];
+
+// Préparer la requête SQL pour insérer une recette
+$sql = "INSERT INTO recettes (id, nom, difficulte, idcategorie) VALUES (?, ?, ?, ?)";
+
+// Préparer la déclaration SQL
+$stmt = $conn->prepare($sql);
+
+// Lier les paramètres
+$stmt->bind_param("issi", $id, $nom, $difficulte, $idcategorie);
+
+// Exécuter la requête
+if ($stmt->execute()) {
+    echo "La recette a été ajoutée avec succès.";
+} else {
+    echo "Erreur lors de l'ajout de la recette : " . $stmt->error;
+}
+
+// Fermer la déclaration et la connexion
+$stmt->close();
+$conn->close();
+
+?>
