53d401479d8478a4c36d406b276128b93f17ec79
satosakisato <121882046+satosakisato@users.noreply.github.com>
2023-12-01 10:52:56 +0100

    feat Ajout d'une barre déroulante et du link css

--- Recette.php
+++ Recette.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recettes</title>
+    <link rel="stylesheet" href="Recette.css">
 </head>
 <body>
-    <h2><a href="Accueil.php">Accueil</a></h2>
+    <h2 class="h2"><a href="Accueil.php">Accueil</a></h2>
+    <form action="Recette.php" method="POST">
+        <select id="choix" name="choix">
+            <option value="Tout">Tout</option>
+            <option value="entré">Entré</option>
+            <option value="plat">Plat</option>
+            <option value="dessert">Dessert</option>
+        </select>
+        <button type="submit">Valider</button>
+    </form>
 </body>
 </html>
 <?php
