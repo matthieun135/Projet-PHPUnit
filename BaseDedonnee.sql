4fed18707435e6a63d34c5a676ca8d4e2571adc3
satosakisato <121882046+satosakisato@users.noreply.github.com>
2023-12-01 10:38:57 +0100

    Refactor Correction d'orthographe

--- BaseDedonnee.sql
+++ BaseDedonnee.sql
@@ -28,4 +28,4 @@ INSERT INTO categories (nom) VALUES ("plat");
 INSERT INTO categories (nom) VALUES ("dessert");
 
 INSERT INTO recettes (nom,imagerecette,contenue,difficulté,idCategories) VALUES ("Mug cake chocolats","./images/Mug Cake Chocolats","Mettez dans un mug 30g de chocolat et de beurre mettez au micro-onde 30s.",1,3);
-INSERT INTO recettes (nom,imagerecette,contenue,difficulté,idCategories) VALUES ("Pizza Reine" "./images/Pizza Reine","Prennez une pate a pizza rajouter de la sauce tomate puis le jambon, le fromage puis le champignon. mettez au four a 200° pendant 15 minutes.",3,2);
+INSERT INTO recettes (nom,imagerecette,contenue,difficulté,idCategories) VALUES ("Pizza Reine", "./images/Pizza Reine","Prennez une pate a pizza rajouter de la sauce tomate puis le jambon, le fromage puis le champignon. mettez au four a 200° pendant 15 minutes.",3,2);
