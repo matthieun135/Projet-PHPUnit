338fab8b98ef0f246e2d088fd24414b90505d5ed
matthieun135 <110544980+matthieun135@users.noreply.github.com>
2023-12-01 15:13:44 +0100

    Add files via upload

--- AjoutRecettes.php
+++ AjoutRecettes.php
@@ -10,14 +10,15 @@
 <body>
     <h2 class="h2"><a href="Accueil.php">Accueil</a></h2>
 <form action="AjoutRecettes.php" method="POST">
-    <input type="text"> nom de la recette</input>
+    nom de la recette
+    <input type="text"> </input>
     <p></p>
-    <input type="file" id="" name="">
+    <input type="file" id="image" name="image">
     <p></p>
 
     difficulté
-    <select id="choix" name="choix">
-            <option value=""></option>
+    <select id="choix1" name="choix1">
+            
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -28,28 +29,37 @@
     <p></p>
     
     catégorie
-    <select id="choix" name="choix">
-            <option value=""></option>
-            <option value="entré">Entrée</option>
-            <option value="plat">Plat</option>
-            <option value="dessert">Dessert</option>
+    <select id="choix2" name="choix2">
+            <option value="1">Entrée</option>
+            <option value="2">Plat</option>
+            <option value="3">Dessert</option>
         </select>
         <!-- <button type="submit">Valider</button> -->
-    </form>
+    
     <p></p>
-    <input type="text"> ingredients</input>
+    ingredients <input type="text"></input>
     <p></p>
     <!-- <input type="time"> temps de preparation</input> -->
 
-    <button type="submit">Valider</button>
+    <button type="submit" name= "Valider" value="Valider">Valider</button>
+</form>
 </body>
 </html>
 
 
 <?php
 
-include ('connexion.php')
+include ('connexion.php');
 
 
+if (isset($_POST['Valider'])){
+    
+    $sql = "INSERT INTO recettes (id, nom, imagerecette, difficulte, idcategorie) VALUES (?,? ,?, ?, ?)";
 
+    if (mysqli_query($conn, $sql)) {
+        echo "La recette a été ajoutée avec succès.";
+    } else {
+        echo "Erreur lors de l'ajout de la recette : " . $sql->error;
+    }
+}
 ?>
