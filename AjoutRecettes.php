c41990d7794e31c4509da01ef1d28afbf5a66620
matthieun135 <110544980+matthieun135@users.noreply.github.com>
2023-12-01 11:42:00 +0100

    conception du html de la page d'ajout

--- AjoutRecettes.php
+++ AjoutRecettes.php
@@ -1,32 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recettes</title>
+</head>
+<body>
+    <h2 class="h2"><a href="Accueil.php">Accueil</a></h2>
+
+    <input type="text"> nom de la recette</input>
+    <p></p>
+    <input type="url"> lien de l'image</input>
+    <p></p>
+    <input type="text"> difficulté</input>
+    <p></p>
+    
+    <form action="AjoutRecettes.php" method="post">catégorie
+    <select id="choix" name="choix">
+            <option value=""></option>
+            <option value="entré">Entrée</option>
+            <option value="plat">Plat</option>
+            <option value="dessert">Dessert</option>
+        </select>
+        <!-- <button type="submit">Valider</button> -->
+    </form>
+    <p></p>
+    <input type="text"> ingredients</input>
+    <p></p>
+    <!-- <input type="time"> temps de preparation</input> -->
+
+</body>
+</html>
+
+
 <?php
 
-require_once 'config.php';
 
-// Récupérer les données du formulaire
-$id = $_POST['id'];
-$nom = $_POST['nom'];
-$image= $_POST['imagerecette'];
-$difficulte = $_POST['difficulte'];
-$idcategorie = $_POST['idcategorie'];
 
-// Préparer la requête SQL pour insérer une recette
-$sql = "INSERT INTO recettes (id, nom, imagerecette, difficulte, idcategorie) VALUES (?,? ,?, ?, ?)";
 
-// Préparer la déclaration SQL
-$stmt = $conn->prepare($sql);
-
-// Lier les paramètres
-$stmt->bind_param("issi", $id, $nom, $image ,$difficulte, $idcategorie);
-
-// Exécuter la requête
-if ($stmt->execute()) {
-    echo "La recette a été ajoutée avec succès.";
-} else {
-    echo "Erreur lors de l'ajout de la recette : " . $stmt->error;
-}
-
-// Fermer la déclaration et la connexion
-$stmt->close();
-$conn->close();
 
 ?>
