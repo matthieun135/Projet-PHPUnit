c944c114839b56d8c3b72acb17fe239fe6849b93
matthieun135 <110544980+matthieun135@users.noreply.github.com>
2023-12-01 16:13:09 +0100

    ajout de la fonction pour ajouter les recettes

--- AjoutRecettes.php
+++ AjoutRecettes.php
@@ -1,46 +1,60 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="AjoutRecettes.css">
         <title>Recettes</title>
     </head>
     <body>
         <h2 class="h2"><a href="Accueil.php">Accueil</a></h2>
         <form action="AjoutRecettes.php" method="POST">
-            nom de la recette
+            <div class="titre">
+                <h1>Ajouter une recette</h1>
+            </div>
+            <div class="nom">
+            nom de la recette :
             <input type="text" name="nom"> </input>
+            </div>
             <p></p>
+            <div class="image">
             Image :
             <input type="file" id="image" name="image">
+            </div>
             <p></p>
-        
-            difficulté
+            <div class="difficulte">
+            difficulté :
             <select id="choix1" name="difficulte">
+                    
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>
-        
+            </div>
             <p></p>
             
-            catégorie
+            <div class="categorie">
+            catégorie :
             <select id="choix2" name="categorie">
                     <option value="1">Entrée</option>
                     <option value="2">Plat</option>
                     <option value="3">Dessert</option>
             </select>
-            
+            </div>
             <p></p>
-            ingredients <input type="text" name ="ingredient"></input>
+            <div class="ingredient">
+            ingredients : <input type="text" name ="ingredient"></input>
+            </div>
             <p></p>
-            Contenue <input type="text" name ="Contenue"></input>
+            <div class="contenue">
+            Contenue : <input type="text" name ="Contenue"></input>
+            </div>
             <p></p>
-        
+            <div class="submit">
             <button type="submit" name= "Valider" value="Valider">Valider</button>
+            </div>
         </form>
     </body>
 </html>
@@ -49,7 +63,6 @@
 <?php
 
 include ('connexion.php');
-
 
 if (isset($_POST['Valider'])){
     $sql = "INSERT INTO recettes (nom, imagerecette, contenue,difficulté, idCategories) VALUES (?, ? ,?, ?, ?)";
