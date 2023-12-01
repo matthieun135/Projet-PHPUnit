41fa9b3979f9145792c96cd16ccbe86afa170138
Nakavasi <119505794+Nakavasi@users.noreply.github.com>
2023-12-01 10:33:51 +0100

    Fix
    Dernier push mal fait correction de ce dernier.

--- Recette.php
+++ Recette.php
@@ -3,26 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2><a href="Accueil.php">Accueil</a></h2>
-</body>
-</html>
-<?php
-include("connexion.php");
-$requete= $connexion->query("SELECT * FROM recettes");
-$recetes= $requete->fetchAll(PDO::FETCH_ASSOC);
-for ($i=0; $i < count($recetes); $i++) { 
-    $image=$recetes[$i]["imagerecette"];
-    $nom=$recetes[$i]["nom"];
-    $nombreetoilesJaune= str_repeat("★ ",$recetes[$i]["difficulté"]);
-    $nombreetoilesGrise=str_repeat("☆ ",5-$recetes[$i]["difficulté"]);
-    $EtoilesDifficulte= $nombreetoilesJaune. $nombreetoilesGrise;<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recettes</title>
 </head>
 <body>
@@ -42,6 +22,7 @@ for ($i=0; $i < count($recetes); $i++) {
 if (isset($_POST['choix'])){
     $choix=$_POST["choix"];
     if ($choix=="Tout"){
+        echo("<h3>Voici toute les recettes.</h3>");
         include("connexion.php");
         $requete= $connexion->query("SELECT * FROM recettes");
         $recetes= $requete->fetchAll(PDO::FETCH_ASSOC);
@@ -85,6 +66,7 @@ if (isset($_POST['choix'])){
 }
 else{
     include("connexion.php");
+    echo("<h3>Voici toute les recettes.</h3>");
     $requete= $connexion->query("SELECT * FROM recettes");
     $recetes= $requete->fetchAll(PDO::FETCH_ASSOC);
     for ($i=0; $i < count($recetes); $i++) { 
@@ -101,13 +83,4 @@ else{
         echo("</div>");
     }
 }
-?>
-    echo("<div class='Recette'>");
-    echo("<h2>Recette n°".($i+1)." ".$nom."</h2>");
-    echo("<br/><img src='".$image."' alt ='Image de ".$nom."'></br>");
-    echo("<h3>Difficulté : ". $EtoilesDifficulte ."</h3>");
-    echo("<a href='AfficherRecette.php?ID=".$recetes[$i]["id"]."'>Contenue de la recette.</a>");
-    echo("</div>");
-}
-
 ?>
